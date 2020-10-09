acb642dddf6066eb276d599b27b813182a66e035
marvin <marvin23m@gmail.com>
2020-10-09 15:45:47 +0100

    Refactor and create/use delete button to delete todo's

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 let todoList = {
     todos: [],
     addTodo: function(todoText) {
@@ -57,10 +55,8 @@ let handlers = {
         changeTodoTextInput.value = '';
         view.displayTodos();
     },
-    deleteTodo: function () {
-        let deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
-        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-        deleteTodoPositionInput.value = '';
+    deleteTodo: function (position) {
+        todoList.deleteTodo(position);
         view.displayTodos();
     },
     toggleCompleted: function () {
@@ -91,8 +87,29 @@ let view = {
                 todoTextWithCompletion = "( ) " + todo.todoText;
             }
 
+            todoLi.id = i;
             todoLi.textContent = todoTextWithCompletion;
+            todoLi.appendChild(this.createDeleteButton())
             todosUl.appendChild(todoLi);
         }
+    },
+    createDeleteButton: function () {
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'deleteButton';
+        return deleteButton;
+    },
+    setUpEventListeners: function () {
+        let todosUl = document.querySelector('ul');
+
+        todosUl.addEventListener('click', function (event) {
+            let elementClicked = event.target;
+
+            if (elementClicked.className === 'deleteButton') {
+                handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+            }
+        })
     }
 };
+
+view.setUpEventListeners();
